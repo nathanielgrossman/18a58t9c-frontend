e957fdc5614f448eded27665d0534a4fcdc2e941
nathanielgrossman <nategrossman@mac.com>
2019-02-04 17:08:44 -0800

    made changes to structure of Image component to fix spacing on mobile

--- src/components/Image.js
+++ src/components/Image.js
@@ -15,13 +15,18 @@ function Image() {
     <div 
       id="image-view" 
       style={bgStyle} 
+      onClick={deselect}
     >
-      <button autoFocus className="image-view-button" onClick={deselect}>
+      <button 
+        autoFocus 
+        id="image-view-button" 
+        onClick={deselect} 
+      />
         <picture className="disable">
           <source className="picture disable" type="image/webp" srcSet={`https://s3-us-west-1.amazonaws.com/18a58t9c/webp/${_id}.webp`} />
           <img className="picture disable" alt={`${original}`} src={`https://s3-us-west-1.amazonaws.com/18a58t9c/jpg/${_id}.jpg`} />
         </picture>
-      </button>
+
     </div>
   )
 }
