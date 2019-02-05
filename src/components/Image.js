5a7d94e953957e53e05ea91be189b8908c8c1295
nathanielgrossman <nategrossman@mac.com>
2019-02-04 17:11:05 -0800

    cleaned up jsx

--- src/components/Image.js
+++ src/components/Image.js
@@ -17,16 +17,11 @@ function Image() {
       style={bgStyle} 
       onClick={deselect}
     >
-      <button 
-        autoFocus 
-        id="image-view-button" 
-        onClick={deselect} 
-      />
-        <picture className="disable">
-          <source className="picture disable" type="image/webp" srcSet={`https://s3-us-west-1.amazonaws.com/18a58t9c/webp/${_id}.webp`} />
-          <img className="picture disable" alt={`${original}`} src={`https://s3-us-west-1.amazonaws.com/18a58t9c/jpg/${_id}.jpg`} />
-        </picture>
-
+      <button autoFocus id="image-view-button" onClick={deselect} />
+      <picture className="disable">
+        <source className="picture disable" type="image/webp" srcSet={`https://s3-us-west-1.amazonaws.com/18a58t9c/webp/${_id}.webp`} />
+        <img className="picture disable" alt={`${original}`} src={`https://s3-us-west-1.amazonaws.com/18a58t9c/jpg/${_id}.jpg`} />
+      </picture>
     </div>
   )
 }
